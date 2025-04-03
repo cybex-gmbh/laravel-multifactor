cf494d86f32ba0bcc4109ad58d24507bb991eb00
Fabian Holy <fabian.holy@cybex-online.com>
2025-04-03 10:05:40 +0200

    (wip) add custom multi-factor authentication views, vite and svg component

--- resources/views/choose-method-custom.blade.php
+++ resources/views/choose-method-custom.blade.php
@@ -2,16 +2,18 @@
     <x-slot name="title">Choose 2FA Method</x-slot>
 
     <div class="container">
-        <div class="center">
-            <h1 class="align-center">Multi-factor Authentication</h1>
-            <p class="align-center">Choose one of these methods to log in</p>
+        <div class="main">
+            <h1>Multi-factor Authentication</h1>
+            <p>Choose one of these methods to log in</p>
             @foreach($userMethods as $method)
-                <a class="btn" href="{{ route('2fa.method', $method) }}">
-                <div class="flex-container">
-                    <x-svg method="{{ $method }}"></x-svg>
-                    <p><strong>{{ ucfirst($method->value) }}</strong></p>
+                <div>
+                    <a class="btn" href="{{ route('2fa.method', $method) }}">
+                        <div class="flex-container">
+                            <x-svg method="{{ $method }}"></x-svg>
+                            <p><strong>{{ ucfirst($method->value) }}</strong></p>
+                        </div>
+                    </a>
                 </div>
-                </a>
             @endforeach
         </div>
     </div>
