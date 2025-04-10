009a1801027fe99396a9af5acf2b6412efc01d36
Fabian Holy <fabian.holy@cybex-online.com>
2025-04-10 12:07:35 +0200

    (wip) fix some bugs in LimitTwoFactorAuthAccessMiddleware

--- src/Http/Middleware/LimitTwoFactorAuthAccess.php
+++ src/Http/Middleware/LimitTwoFactorAuthAccess.php
@@ -3,6 +3,7 @@
 namespace CybexGmbh\LaravelTwoFactor\Http\Middleware;
 
 use Closure;
+use CybexGmbh\LaravelTwoFactor\Enums\TwoFactorAuthMode;
 use CybexGmbh\LaravelTwoFactor\Enums\TwoFactorAuthSession;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,11 +21,19 @@ class LimitTwoFactorAuthAccess
         $method = $request->route('method');
         $isVerified = TwoFactorAuthSession::VERIFIED->get();
 
-        if (($method->isAllowed() || !$method->isUserMethod()) && !$isVerified) {
+        if (($method->isAllowed() && !$method->isUserMethod()) && !$isVerified) {
+            return redirect()->route('2fa.show');
+        }
+
+        if (!$method->isAllowed() && !$method->isUserMethod() && !$isVerified) {
             return redirect()->route('2fa.show');
         }
 
         if ((!$method->isAllowed() || $method->isUserMethod()) && $isVerified) {
+            if (TwoFactorAuthMode::fromConfig() === TwoFactorAuthMode::FORCE) {
+                return redirect()->back();
+            }
+
             return redirect()->route('2fa.settings', Auth::user());
         }
 
