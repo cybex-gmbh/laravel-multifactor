efeb06695cf6bffb78f9192931e92b9a6c35eec9
Fabian Holy <fabian.holy@cybex-online.com>
2025-04-11 11:15:03 +0200

    refactor LimitTwoFactorAuthAccess middleware

--- src/Http/Middleware/LimitTwoFactorAuthAccess.php
+++ src/Http/Middleware/LimitTwoFactorAuthAccess.php
@@ -21,15 +21,11 @@ class LimitTwoFactorAuthAccess
         $method = $request->route('method');
         $isVerified = TwoFactorAuthSession::VERIFIED->get();
 
-        if (($method->isAllowed() && !$method->isUserMethod()) && !$isVerified) {
+        if (!$isVerified && !$method->isUserMethod()) {
             return redirect()->route('2fa.show');
         }
 
-        if (!$method->isAllowed() && !$method->isUserMethod() && !$isVerified) {
-            return redirect()->route('2fa.show');
-        }
-
-        if ((!$method->isAllowed() || $method->isUserMethod()) && $isVerified) {
+        if ($isVerified && (!$method->isAllowed() || $method->isUserMethod())) {
             if (TwoFactorAuthMode::fromConfig() === TwoFactorAuthMode::FORCE) {
                 return redirect()->back();
             }
