8d18c39df2812e102fdf1dbb066e06ada58e04b8
Fabian Holy <fabian.holy@cybex-online.com>
2025-06-23 15:55:39 +0200

    add translations

--- resources/views/email-challenge.blade.php
+++ resources/views/email-challenge.blade.php
@@ -1,9 +1,9 @@
 <x-multi-factor::layout>
-    <x-slot name="title">Two Factor Login</x-slot>
+    <x-slot name="title">@lang('multi-factor::auth.title')</x-slot>
 
     <x-multi-factor::auth-card>
-        <x-slot name="subtext">
-            <p>An email with an authentication {{ $authenticationMethod }} was just sent to <strong>{{ $user->email }}</strong></p>
+        <x-slot name="subtitle">
+            <p>@lang('multi-factor::auth.email_challenge.subtitle', ['authenticationMethod' => $authenticationMethod, 'email' => $user->email])</p>
         </x-slot>
 
         <x-multi-factor::form :action="route('mfa.verify', $mfaMethod, $user)" id="2fa-login" class="mt-4">
@@ -11,10 +11,10 @@
         </x-multi-factor::form>
 
         <div class="flex flex-row flex-end">
-            <p class="text-sm">Didn't receive your {{ $authenticationMethod }}?</p>
+            <p class="text-sm">@lang('multi-factor::auth.email_challenge.subtitle_resend', ['authenticationMethod' => $authenticationMethod])</p>
 
             <x-multi-factor::form :action="route('mfa.method.send', $mfaMethod)" id="resend-code-form" class="underline-inline-primary">
-                <x-multi-factor::button class="text-sm">{{ __(sprintf('Resend %s', $authenticationMethod)) }}</x-multi-factor::button>
+                <x-multi-factor::button class="text-sm">@lang('multi-factor::button.resend_mfa', ['authenticationMethod' => $authenticationMethod])</x-multi-factor::button>
             </x-multi-factor::form>
 
             <x-multi-factor::button type="submit" class="text-sm" form="2fa-login">
