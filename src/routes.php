565e981f03608791238adfbd0d031bc7732f4ca6
Fabian Holy <fabian.holy@cybex-online.com>
2025-06-25 15:15:57 +0200

    rename mfa controller methods

--- src/routes.php
+++ src/routes.php
@@ -14,19 +14,19 @@ Route::middleware(['web'])->group(function () {
     Route::middleware(['auth'])->as('mfa.')->group(function () {
         if (config('multi-factor.features.settings.enabled') && $path = config('multi-factor.features.settings.routePath')) {
             Route::middleware(['hasMultiFactorAuthentication', 'hasAllowedMultiFactorAuthMethods'])->group(function () use ($path) {
-                Route::get($path, [MultiFactorAuthController::class, 'twoFactorSettings'])->name('settings');
+                Route::get($path, [MultiFactorAuthController::class, 'multiFactorSettings'])->name('settings');
             });
         }
 
         Route::prefix('mfa')->group(function () {
-            Route::delete('delete/{method}', [MultiFactorAuthController::class, 'deleteTwoFactorAuthMethod'])->name('delete.method');
+            Route::delete('delete/{method}', [MultiFactorAuthController::class, 'deleteMultiFactorAuthMethod'])->name('delete.method');
             Route::get('setup/{method?}', [MultiFactorAuthController::class, 'setup'])->name('setup');
 
             Route::middleware(['redirectIfMultiFactorAuthenticated'])->group(function () {
                 Route::get('', [MultiFactorAuthController::class, 'show'])->name('show');
 
                 Route::middleware(['limitMultiFactorAuthAccess'])->group(function () {
-                    Route::get('{method}', [MultiFactorAuthController::class, 'handleTwoFactorAuthMethod'])->name('method');
+                    Route::get('{method}', [MultiFactorAuthController::class, 'handleMultiFactorAuthMethod'])->name('method');
                     Route::post('{method}/send', [MultiFactorAuthController::class, 'send'])->name('method.send');
                     Route::post('{method}/verify', [MultiFactorAuthController::class, 'verifyTwoFactorAuthCode'])->name('verify');
                     Route::get('{method}/login/{user}/{code}', [MultiFactorAuthController::class, 'verifyTwoFactorAuthCode'])->middleware('signed')->name('login');
