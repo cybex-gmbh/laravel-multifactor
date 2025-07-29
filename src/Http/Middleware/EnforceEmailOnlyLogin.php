566afe5d9ac7b2c5c16e41c1be8ab4bfe037db42
Fabian Holy <fabian.holy@cybex-online.com>
2025-07-29 11:17:09 +0200

    throw exceptions if email-only login mode is misconfigured

--- src/Http/Middleware/EnforceEmailOnlyLogin.php
+++ src/Http/Middleware/EnforceEmailOnlyLogin.php
@@ -4,22 +4,13 @@ namespace Cybex\LaravelMultiFactor\Http\Middleware;
 
 use Closure;
 use Cybex\LaravelMultiFactor\Contracts\MultiFactorLoginViewResponseContract;
-use Cybex\LaravelMultiFactor\Enums\MultiFactorAuthMethod;
-use Cybex\LaravelMultiFactor\Enums\MultiFactorAuthMode;
 use Illuminate\Http\Request;
-use MFA;
 
 class EnforceEmailOnlyLogin
 {
     public function handle(Request $request, Closure $next)
     {
-        if (MFA::isEmailOnlyLoginActive() && MultiFactorAuthMode::isForceMode() && MFA::getForceMethod(
-            ) === MultiFactorAuthMethod::EMAIL) {
-
-            $response = app(MultiFactorLoginViewResponseContract::class);
-            return response($response->toResponse($request));
-        }
-
-        return $next($request);
+        $response = app(MultiFactorLoginViewResponseContract::class);
+        return response($response->toResponse($request));
     }
 }
